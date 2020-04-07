9e450fd56b5b2e73d435cd8b27d822dfcf907430
Yevhenii Varnavskyi <eugenev112@gmail.com>
2020-04-07 12:29:13 +0300

    Fix initializer

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 if ENV['ELASTICSEARCH_HOST']
-  Article.__elasticsearch__client = ElasticSearch::Client.new(host: ENV['ELASTICSEARCH_HOST'])
+  Elasticsearch::Model.client = Elasticsearch::Client.new(host: ENV['ELASTICSEARCH_HOST'])
 end
